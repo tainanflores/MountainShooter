2702924a2ea600cd24d411d752ee5d148090af67
Tainan Flores <tainan1317@yahoo.com.br>
2026-06-27 01:47:04 -0300

    Add quit message before closing the window

--- main.py
+++ main.py
@@ -9,5 +9,6 @@ while True:
     # Check for all events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            print('Quitting...')
             pygame.quit()  # Close Window
             quit()
