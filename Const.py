f1136ca4f0a5256cbec1fa549b62e89e392686a4
Tainan Flores <tainan1317@yahoo.com.br>
2026-06-29 12:15:02 -0300

    Level 2 implementation

--- Const.py
+++ Const.py
@@ -1,5 +1,5 @@
-
 import pygame
+
 # C
 C_ORANGE = (255, 128, 0)
 C_WHITE = (255, 255, 255)
@@ -16,6 +16,11 @@ ENTITY_SPEED = {
     'Level1Bg4': 4,
     'Level1Bg5': 5,
     'Level1Bg6': 6,
+    'Level2Bg0': 0,
+    'Level2Bg1': 1,
+    'Level2Bg2': 2,
+    'Level2Bg3': 3,
+    'Level2Bg4': 4,
     'Player1': 3,
     'Player1Shot': 1,
     'Player2': 3,
@@ -33,6 +38,11 @@ ENTITY_DAMAGE = {
     'Level1Bg4': 0,
     'Level1Bg5': 0,
     'Level1Bg6': 0,
+    'Level2Bg0': 0,
+    'Level2Bg1': 0,
+    'Level2Bg2': 0,
+    'Level2Bg3': 0,
+    'Level2Bg4': 0,
     'Player1': 1,
     'Player1Shot': 25,
     'Player2': 1,
@@ -50,6 +60,11 @@ ENTITY_HEALTH = {
     'Level1Bg4': 999,
     'Level1Bg5': 999,
     'Level1Bg6': 999,
+    'Level2Bg0': 999,
+    'Level2Bg1': 999,
+    'Level2Bg2': 999,
+    'Level2Bg3': 999,
+    'Level2Bg4': 999,
     'Player1': 300,
     'Player1Shot': 1,
     'Player2': 300,
@@ -68,6 +83,11 @@ ENTITY_SCORE = {
     'Level1Bg4': 0,
     'Level1Bg5': 0,
     'Level1Bg6': 0,
+    'Level2Bg0': 0,
+    'Level2Bg1': 0,
+    'Level2Bg2': 0,
+    'Level2Bg3': 0,
+    'Level2Bg4': 0,
     'Player1': 0,
     'Player1Shot': 0,
     'Player2': 0,
@@ -78,12 +98,13 @@ ENTITY_SCORE = {
     'Enemy2Shot': 0,
 }
 ENTITY_SHOT_DELAY = {
-    'Player1':20,
-    'Player2':20,
+    'Player1': 20,
+    'Player2': 20,
     'Enemy1': 100,
     'Enemy2': 200,
 }
 EVENT_ENEMY = pygame.USEREVENT + 1
+EVENT_TIMEOUT = pygame.USEREVENT + 2
 
 # M
 MENU_OPTION = ('NEW GAME 1P',
@@ -106,6 +127,10 @@ PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
 
 # S
 SPAWN_TIME = 4000
+
+# T
+TIMEOUT_LEVEL = 20000
+TIMEOUT_STEP = 100
 
 # W
 WIN_WIDTH = 576
